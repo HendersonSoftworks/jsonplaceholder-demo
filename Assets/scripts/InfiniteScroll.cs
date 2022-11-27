f3a66b03231cdf06dc40598eee7a9adc08782a3a
HendersonSoftworks <thomas.henderson@hendersonsoftworks.com>
2022-11-27 13:17:49 +0000

    spawning in 20 cards works correctly

--- Assets/scripts/InfiniteScroll.cs
+++ Assets/scripts/InfiniteScroll.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.EventSystems;
 
 public class InfiniteScroll : MonoBehaviour
 {
@@ -12,14 +13,31 @@ public class InfiniteScroll : MonoBehaviour
     [SerializeField] private GameObject infiniteScrollPage;
     [SerializeField] private GameObject verticalCardBase;
     [SerializeField] private GameObject contentPanel;
+
+    [SerializeField] private ScrollRect scrollRect;
     [SerializeField] private Scrollbar scrollbarVert;
+    
 
     private void Update()
     {
-        //Canvas.ForceUpdateCanvases();
-
+        // Show scrollval in editor for debugging
         scrollVal = scrollbarVert.value;
         
-        
+        // Check if user has reached the end of the page, if so, spawn 20 more cards and force the canvas to update.
+        if (scrollVal <= 0.1)
+        {
+            AddMoreCards();
+            scrollVal = 1; // reset scrollval to 1 temporarily to avoid spawning more cards than necessary. It would probably be better to implement event handlers here.
+            Canvas.ForceUpdateCanvases();
+        }
+    }
+
+    private void AddMoreCards()
+    {
+        for (int i = 0; i < 20; i++)
+        {
+            GameObject newCard = Instantiate(verticalCardBase);
+            newCard.transform.SetParent(contentPanel.transform, false);
+        }
     }
 }
