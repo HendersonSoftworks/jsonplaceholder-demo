a298791df2aea4a6917e5ab85e38d0e7351fbde4
HendersonSoftworks <thomas.henderson@hendersonsoftworks.com>
2022-11-27 20:14:38 +0000

    Radial menu navigation completed

--- Assets/scripts/ContentManager.cs
+++ Assets/scripts/ContentManager.cs
@@ -21,6 +21,8 @@ public class ContentManager : MonoBehaviour
         animationPage.SetActive(false);
         infiniteScrollPage.SetActive(true);
         model.SetActive(false);
+        radialPage.SetActive(false);
+
     }
 
     public void ActivatePage2()
@@ -31,5 +33,19 @@ public class ContentManager : MonoBehaviour
         animationPage.SetActive(true);
         infiniteScrollPage.SetActive(false);
         model.SetActive(true);
+        radialPage.SetActive(false);
+
     }
+
+    public void ActivatePage3()
+    {
+        pageNum.text = "3";
+        pageName.text = "radial Menu";
+
+        animationPage.SetActive(false);
+        infiniteScrollPage.SetActive(false);
+        model.SetActive(false);
+        radialPage.SetActive(true);
+    }
+
 }
